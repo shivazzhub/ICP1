542095c1a7506f5d0600dc5bba476c5bf868d6a8
shivazzhub <shivasaiyembari@gmail.com>
2024-06-24 16:36:08 -0500

    Create Grade.py
    3

---
+++ Grade.py
@@ -0,0 +1,17 @@
+Score = int(input())
+while Score<=100:
+  if Score>=90:
+    print("Grade A")
+    break
+  elif Score>=80:
+    print("Grade B")
+    break
+  elif Score>=70:
+    print("Grade C")
+    break
+  elif Score>=60:
+    print("Grade D")
+    break
+  else:
+    print("Grade F")
+    break
